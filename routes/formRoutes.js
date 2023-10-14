ef6b3178888db43063f3fd5d8899ea79e6107b77
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-10-14 16:39:34 -0700

    srihari new routes done

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,16 +1,23 @@
 import express from 'express';
-import  { createForm, getAllForms, getAllQuestions, getAllResponses } from '../controllers/formController.js';
+import  { getAllQuestionsForOneForm, getAllFormsForCompany, getFormByID, createForm, getAllForms, getAllQuestions, getAllResponses } from '../controllers/formController.js';
 
 const router = express.Router();
 
 router.route('/')
     .post(createForm)
+    .get(getFormByID)
 
 router.route('/all')
     .get(getAllForms)
 
 router.route('/questions/all')
     .get(getAllQuestions)
+
+router.route('/questions')
+    .get(getAllQuestionsForOneForm)
+
+router.route('/employer')
+    .get(getAllFormsForCompany)
 
 router.route('/responses/all')
     .get(getAllResponses)
