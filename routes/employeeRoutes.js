3a482d4b618273cddb78845e3cb8d0f47e5fc03d
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-10-14 12:29:56 -0700

    first commit

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import  { createEmployee, getAllEmployees } from '../controllers/employeesController.js';
+
+const router = express.Router();
+
+router.route('/')
+    .post(createEmployee)
+
+router.route('/all')
+    .get(getAllEmployees)
+
+export default router;
