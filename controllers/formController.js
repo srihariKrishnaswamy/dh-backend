f7635d042e30169140edf90ed68625cdbfeef23a
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-10-14 19:58:27 -0700

    aoirgaerion

--- controllers/formController.js
+++ controllers/formController.js
@@ -154,7 +154,7 @@ export const submitForm = asyncHandler(async (req, res) => {
   const { employee_id, answers } = req.body; // no form_id needed, kinda sketch
   // answers = {question_id: number, question_id: number}
   for (let key of Object.keys(answers)) {
-    var keyInt = parseInt(key)
+    var keyInt = parseInt(key);
     await pool.query(
       `
             INSERT INTO response (answer, question_id, employee_id)
@@ -171,85 +171,127 @@ export const submitForm = asyncHandler(async (req, res) => {
       [keyInt]
     );
   }
-  res.status(200).json({message: "Form filled!"});
+  res.status(200).json({ message: "Form filled!" });
 });
 
 export const findIfEmployeeFilledForm = asyncHandler(async (req, res) => {
   var { employee_id, form_id } = req.query;
-  const [qIDs] = await pool.query(`
+  const [qIDs] = await pool.query(
+    `
     SELECT question_id FROM question WHERE form_id = ?
-  `, [form_id])
+  `,
+    [form_id]
+  );
   employee_id = parseInt(employee_id);
   for (var qID of qIDs) {
     console.log(qID);
     console.log(employee_id);
-    qID = qID['question_id'];
-    const [found] = await pool.query(`
+    qID = qID["question_id"];
+    const [found] = await pool.query(
+      `
     SELECT * FROM response WHERE question_id = ? AND employee_id = ?
-    `, [qID, employee_id])
+    `,
+      [qID, employee_id]
+    );
     console.log(found);
     if (found.length > 0) {
-        res.status(200).json({found: true});
-        return;
+      res.status(200).json({ found: true });
+      return;
     }
   }
-  res.status(200).json({found: false});
+  res.status(200).json({ found: false });
 });
 
 export const updateTitle = asyncHandler(async (req, res) => {
-    const {form_id, title} = req.body;
-    await pool.query('UPDATE form SET title = ? WHERE form_id = ?', [title, form_id])
-    const [rows] = await pool.query('SELECT * FROM form WHERE form_id = ?', [form_id])
-    res.status(200).json(rows[0])
-})
+  const { form_id, title } = req.body;
+  await pool.query("UPDATE form SET title = ? WHERE form_id = ?", [
+    title,
+    form_id,
+  ]);
+  const [rows] = await pool.query("SELECT * FROM form WHERE form_id = ?", [
+    form_id,
+  ]);
+  res.status(200).json(rows[0]);
+});
 
 export const updateDescription = asyncHandler(async (req, res) => {
-    const {form_id, form_description} = req.body;
-    await pool.query('UPDATE form SET form_description = ? WHERE form_id = ?', [form_description, form_id])
-    const [rows] = await pool.query('SELECT * FROM form WHERE form_id = ?', [form_id])
-    res.status(200).json(rows[0])
-})
+  const { form_id, form_description } = req.body;
+  await pool.query("UPDATE form SET form_description = ? WHERE form_id = ?", [
+    form_description,
+    form_id,
+  ]);
+  const [rows] = await pool.query("SELECT * FROM form WHERE form_id = ?", [
+    form_id,
+  ]);
+  res.status(200).json(rows[0]);
+});
 
 export const updateCurrentDate = asyncHandler(async (req, res) => {
-    const {form_id, curr_date} = req.body;
-    await pool.query('UPDATE form SET curr_date = ? WHERE form_id = ?', [curr_date, form_id])
-    const [rows] = await pool.query('SELECT * FROM form WHERE form_id = ?', [form_id])
-    res.status(200).json(rows[0])
-})
+  const { form_id, curr_date } = req.body;
+  await pool.query("UPDATE form SET curr_date = ? WHERE form_id = ?", [
+    curr_date,
+    form_id,
+  ]);
+  const [rows] = await pool.query("SELECT * FROM form WHERE form_id = ?", [
+    form_id,
+  ]);
+  res.status(200).json(rows[0]);
+});
+
+const getAvgs = async (form_id) => {
+  const [questions] = await pool.query(
+    `
+    SELECT * FROM question WHERE form_id = ?
+`,
+    [form_id]
+  );
+
+  const [form] = await pool.query(`
+    SELECT * FROM form WHERE form_id = ?
+  `, [form_id])
+
+  var totals = {
+    // total score for each category
+    wlb: 0,
+    transparency: 0,
+    culture: 0,
+    overall: 0,
+    form_id: form_id,
+    title: form[0].title,
+    curr_date: form[0].curr_date
+  };
+  var numRes = {
+    // total # responses for each category
+    wlb: 0,
+    transparency: 0,
+    culture: 0,
+    overall: 0,
+  };
+  for (var question of questions) {
+    var currCat = question.category;
+    var qID = question.question_id;
+    const [responses] = await pool.query(
+      `
+    SELECT * FROM response WHERE question_id = ?
+    `,
+      [qID]
+    );
+    for (var response of responses) {
+      totals[currCat] += response.answer;
+      numRes[currCat] += 1;
+    }
+  }
+  console.log(totals);
+  console.log(numRes);
+  const keys = Object.keys(numRes);
+  for (const key of keys) {
+    totals[key] /= parseFloat(numRes[key]);
+  }
+  return totals;
+};
 
 export const getCatAverages = asyncHandler(async (req, res) => {
-    const {form_id} = req.body;
-    const [questions] = await pool.query(`
-        SELECT * FROM question WHERE form_id = ?
-    `, [form_id])
-    var totals = { // total score for each category
-        wlb: 0,
-        transparency: 0,
-        culture: 0,
-        overall: 0,
-    }
-    var numRes = { // total # responses for each category
-        wlb: 0,
-        transparency: 0,
-        culture: 0,
-        overall: 0,
-    }
-    for (var question of questions) {
-        var currCat = question.category;
-        var qID = question.question_id;
-        const [responses] = await pool.query(`
-        SELECT * FROM response WHERE question_id = ?
-        `, [qID])
-        for (var response of responses) {
-            totals[currCat] += response.answer;
-            numRes[currCat] += 1
-        }
-    }
-    console.log(totals)
-    console.log(numRes)
-    const keys = Object.keys(numRes);
-    for (const key of keys) {
-        totals[key] /= parseFloat(numRes[key])
-    }
-    res.status(200).json(totals);
-})
+  const { form_id } = req.body;
+  const totals = await getAvgs(form_id);
+  res.status(200).json(totals);
+});
